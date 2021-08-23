cfb077f3add2105927e6fc81cf5935e5a458aade
poyanpayandeh1 <poyanpayandeh@gmail.com>
2021-08-24 01:50:05 +1200

    i want cry

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 //hi
 
@@ -22,6 +23,7 @@ export default function App() {
       <Stack.Navigator screenOptions={globalScreenOptions}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDGMBU_WoTrjW0djFS02kmSzy9cfy4M8rw",
   authDomain: "singalclone.firebaseapp.com",
@@ -6,3 +10,16 @@ const firebaseConfig = {
   messagingSenderId: "1030027874737",
   appId: "1:1030027874737:web:f3aaf479b3462d69d1ad90",
 };
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
