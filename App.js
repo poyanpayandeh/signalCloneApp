0f4edff9bf4c5c40f97562c11510c940943574db
poyanpayandeh1 <poyanpayandeh@gmail.com>
2021-08-23 23:06:11 +1200

    heheh i want die

---
+++ App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGMBU_WoTrjW0djFS02kmSzy9cfy4M8rw",
+  authDomain: "singalclone.firebaseapp.com",
+  projectId: "singalclone",
+  storageBucket: "singalclone.appspot.com",
+  messagingSenderId: "1030027874737",
+  appId: "1:1030027874737:web:f3aaf479b3462d69d1ad90",
+};
